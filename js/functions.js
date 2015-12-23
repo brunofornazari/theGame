bfca11feba1caa5b2092196eb6cbb12291ab7906
boiling-peak-2036 <bruno_fornazari@hotmail.com>
2015-12-23 00:20:05 -0200

    0.01

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function msgBox(canvas, content, propriedades){
+	var defaultProperties = {
+		width:"100px",
+		height:"100px",
+		colot:"#CCC",
+		defaultMessage:"Olá Teste"
+	};
+	this.propriedades = propriedades || defaultProperties;
+}
---
+++ js/javatext.js
@@ -0,0 +1,8 @@
+var frame = document.getElementById("frame");
+var body = document.getElementsByTagName("body");
+
+frame.style.width = "100%";
+frame.style.height = "100%";
+
+
+var teste = new msgBox(11, 11, 11);
